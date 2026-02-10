523e3f0d8600fd63f4cae3516d458d3d8e7d6d83
Debankur Saikia <debankur19@gmail.com>
2026-02-10 19:06:32 +0530

    restructured listings and reviews using express router

---
+++ routes/review.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });//agar parent route ke andar kuch parameters hain jo use ho sakte hain child ke andar toh mergeparams : true likhna padta hain
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const { reviewSchema } = require("../schema.js");
+const Review = require("../models/review.js");
+const Listing = require("../models/listing.js");
+
+
+const validateReview = (req, res, next) => {
+    let {error} = reviewSchema.validate(req.body);//schema validation
+    
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
+
+//Reviews
+//POST Review Route
+router.post("/", validateReview, wrapAsync(async(req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();//existing document ke andar kuch naya add karne ke liye save function call karna padta hain
+
+
+    res.redirect(`/listings/${listing._id}`);
+}));
+
+//DELETE Review Route
+router.delete("/:reviewId", wrapAsync(async (req, res) => {
+    let {id, reviewId} = req.params;
+    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});//$pull operator removes from an existing array all instances of a value or value that match a specified condition
+
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+}));
+
+
+module.exports = router;
